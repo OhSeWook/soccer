418bbfe58d8297c7264b806834e19f4434a2b9da
osw1987 <92846046+OhSeWook@users.noreply.github.com>
2023-04-26 20:23:21 +0900

    쉘파일 수정

--- admin/soccer_admin_start.sh
+++ admin/soccer_admin_start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-APP_PORT={7070}
-nohup java -jar -Dserver.port=$APP_PORT /var/lib/jenkins/workspace/soccer_all/admin/target/admin-0.0.1.admin-SNAPSHOT.jar > /dev/null 2>&1 
+nohup java -jar /var/lib/jenkins/workspace/soccer_all/admin/target/admin-0.0.1.admin-SNAPSHOT.jar >> /usr/osw/soccer/admin/logs/soccerAdminLog.log 2>&1 &
