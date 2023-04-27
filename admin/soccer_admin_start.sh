6212c11077c3a53abcc3890fe51bec5698eb566d
osw1987 <92846046+OhSeWook@users.noreply.github.com>
2023-04-27 14:25:56 +0900

    배포 테스트

--- admin/soccer_admin_start.sh
+++ admin/soccer_admin_start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-nohup java -jar /var/lib/jenkins/workspace/soccer_all/admin/target/admin-0.0.1.admin-SNAPSHOT.jar >> /usr/osw/soccer/admin/logs/soccerAdminLog.log 2>&1 &
+nohup java -jar -Dserver.port=7070 /var/lib/jenkins/workspace/soccer_all/admin/target/admin-0.0.1.admin-SNAPSHOT.jar >> /usr/osw/soccer/admin/logs/soccerAdminLog.log 2>&1 &
+
