f0edb3131535894f6fa30ad46febba1e35dba09f
osw1987 <92846046+OhSeWook@users.noreply.github.com>
2023-04-27 14:28:53 +0900

    배포 마지막 테스트

--- admin/soccer_admin_start.sh
+++ admin/soccer_admin_start.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-
 nohup java -jar -Dserver.port=7070 /var/lib/jenkins/workspace/soccer_all/admin/target/admin-0.0.1.admin-SNAPSHOT.jar >> /usr/osw/soccer/admin/logs/soccerAdminLog.log 2>&1 &
-
